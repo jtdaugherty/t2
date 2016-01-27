15dc6d0ff5dc464f6534f59846d944614788ae99
Jonathan Daugherty <cygnus@foobox.com>
2016-01-26 18:05:03 -0800

    texture: when creating texture buffer, initialize with zeros

--- src/texture.c
+++ src/texture.c
@@ -1,12 +1,16 @@
 
 #include <OpenGL/gl.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <t2/texture.h>
 
 GLuint make_texture(int width, int height)
 {
     GLuint texture;
+
+    unsigned char *buf = malloc(width * height * sizeof(float) * 4);
+    memset(buf, 0, width * height * sizeof(float) * 4);
 
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
@@ -19,8 +23,10 @@ GLuint make_texture(int width, int height)
         GL_RGBA,
         width, height, 0,
         GL_RGBA, GL_FLOAT,
-        NULL
+        buf
     );
+
+    free(buf);
 
     return texture;
 }
