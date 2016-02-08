0007804d2f48f25ce9bedd205ca97e9c8247f931
Jonathan Daugherty <cygnus@foobox.com>
2016-02-07 23:23:39 -0800

    Use better names for bit macros

--- src/main.c
+++ src/main.c
@@ -85,8 +85,8 @@ uint8_t button_mask = 0;
 #define ANY_PRESSED     ((button_mask & (KB_PRESSED | MOUSE_PRESSED)) != 0)
 #define NONE_PRESSED    ((button_mask & (KB_PRESSED | MOUSE_PRESSED)) == 0)
 
-#define CLEAR(mask, bit)  do { mask &= ~bit; } while (0);
-#define SET(mask, bit)    do { mask |= bit; } while (0);
+#define CLEAR_BIT(mask, bit)  do { mask &= ~bit; } while (0);
+#define SET_BIT(mask, bit)    do { mask |= bit; } while (0);
 
 static inline void restartRendering()
 {
@@ -222,11 +222,11 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 
     if (action == GLFW_PRESS)
     {
-        SET(button_mask, MOUSE_PRESSED);
+        SET_BIT(button_mask, MOUSE_PRESSED);
         glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
         glfwGetCursorPos(window, &cursorX, &cursorY);
     } else {
-        CLEAR(button_mask, MOUSE_PRESSED);
+        CLEAR_BIT(button_mask, MOUSE_PRESSED);
         glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
     }
 }
@@ -268,9 +268,9 @@ static void key_callback(GLFWwindow* window, int key, int scancode,
 #define INCREASE_SAMPLE_ROOT (PRESS(GLFW_KEY_T) && SHIFT)
 
     if (action == GLFW_PRESS) {
-        SET(button_mask, KB_PRESSED);
+        SET_BIT(button_mask, KB_PRESSED);
     } else if (action == GLFW_RELEASE) {
-        CLEAR(button_mask, KB_PRESSED);
+        CLEAR_BIT(button_mask, KB_PRESSED);
     }
 
     if (DECREASE_SAMPLE_ROOT && config.sampleRoot > 1) {
